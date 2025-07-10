7623bb720cf18c9ceb59645d084cc43168ac2fb4
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-07-10 23:45:28 +0200

    index again

--- index.js
+++ index.js
@@ -4,3 +4,4 @@ var b = 5;
 var isAuthenticate = false;
 var arr = [1, 2, 3, 4, 5];
 console.log(arr.filter(function (n) { return n > 3; }));
+var x = 123;
--- index.ts
+++ index.ts
@@ -1,8 +1,24 @@
 let helloMsg: string = 'Hello, there';
-let a: any = 123;
+let a: number = 123;
 let b: number = 5;
 
 let isAuthenticate: boolean = false;
 
 const arr: number [] = [1, 2, 3, 4, 5];
 console.log(arr.filter((n) => n > 3));
+
+let x: number | string = 123;
+
+interface User ={
+    name: string,
+    age: number,
+    address?: string
+
+}
+
+const peshoUser: User = {
+    name: 'Pesho',
+    age: 20,
+    address: 'Sofia'
+
+};
