3fbf3fa4fda12eab3a95538d48a6528e6a270ef6
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-07-10 23:40:47 +0200

    index

---
+++ index.js
@@ -0,0 +1,6 @@
+var helloMsg = 'Hello, there';
+var a = 123;
+var b = 5;
+var isAuthenticate = false;
+var arr = [1, 2, 3, 4, 5];
+console.log(arr.filter(function (n) { return n > 3; }));
---
+++ index.ts
@@ -0,0 +1,8 @@
+let helloMsg: string = 'Hello, there';
+let a: any = 123;
+let b: number = 5;
+
+let isAuthenticate: boolean = false;
+
+const arr: number [] = [1, 2, 3, 4, 5];
+console.log(arr.filter((n) => n > 3));
