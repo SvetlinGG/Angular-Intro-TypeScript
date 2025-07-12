2ad89626e8ccd1ec740acd1043843f9bf522eba3
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-07-12 23:15:40 +0200

    setup

--- index.ts
+++ index.ts
@@ -9,12 +9,18 @@ console.log(arr.filter((n) => n > 3));
 
 let x: number | string = 123;
 
-interface User ={
-    name: string,
-    age: number,
+// interface User ={
+//     name: string,
+//     age: number,
+//     address?: string
+
+// }
+type User = {
+    name: string, 
+    age: number
     address?: string
 
-}
+};
 
 const peshoUser: User = {
     name: 'Pesho',
